ad09b870003ea03edc18d8a52e82af470f18d018
Itzel Doroteo Mtz <20211311@UTHH.EDU.MX>
2023-10-19 09:56:30 -0600

    Update-Files

--- JS/cifrado.js
+++ JS/cifrado.js
@@ -1,46 +1,46 @@
-var numFilas = 0
-      var numColumnas = 0
+var canFila = 0
+      var canColumna = 0
 
       
-      function generarTabla() {
-          numFilas = parseInt($('#filas').val());
-          numColumnas = parseInt($('#columnas').val());
+      function tablaNueva() {
+          canFila = parseInt($('#filas').val());
+          canColumna = parseInt($('#columnas').val());
           
           var tabla = $('#tablaDinamica');
           var input = $('#input');
   
           tabla.empty().addClass('table table-bordered table-custom');
   
-          for (var i = 0; i < numFilas; i++) {
+          for (var i = 0; i < canFila; i++) {
               var fila = $('<tr>');
-              for (var j = 0; j < numColumnas; j++) {
+              for (var j = 0; j < canColumna; j++) {
                   fila.append($('<td>').css('width', '30px').css('height', '30px').text(''));
               }
               tabla.append(fila);
           }
   
-          // Limitar la cantidad de caracteres en el input a filas * columnas
-          var maxLength = numFilas * numColumnas;
+          // limita la cantidad de caracteres  filas * columnas
+          var maxLength = canFila * canColumna;
           input.attr('maxlength', maxLength);
           input.attr('placeholder', 'Máximo ' + maxLength + ' caracteres');
       }
 
-      function cifrarTexto() {
-        numFilas = parseInt($('#filas').val());
-        numColumnas = parseInt($('#columnas').val());
+      function cifrarMen() {
+        canFila = parseInt($('#filas').val());
+        canColumna = parseInt($('#columnas').val());
         var input = $('#input').val();
         var output = '';
 
         // Verificar si el texto cabe en la tabla
-        if (input.length > numFilas * numColumnas) {
+        if (input.length > canFila * canColumna) {
             alert('El texto es demasiado largo para la tabla proporcionada.');
             return;
         }
 
         // Llenar la tabla y el campo de salida
-        for (var i = 0; i < numColumnas; i++) {
-            for (var j = 0; j < numFilas; j++) {
-                var index = j * numColumnas + i;
+        for (var i = 0; i < canColumna; i++) {
+            for (var j = 0; j < canFila; j++) {
+                var index = j * canColumna + i;
                 if (index < input.length) {
                     $('#tablaDinamica tr:eq(' + j + ') td:eq(' + i + ')').text(input[index]);
                     output += input[index];
@@ -51,23 +51,23 @@ var numFilas = 0
         $('#output').val(output);
     }
 
-    $('#filas, #columnas').on('input', generarTabla);
+    $('#filas, #columnas').on('input', tablaNueva);
 
-    function descifrarTexto() {
-        var numFilas = parseInt($('#filas').val());
-        var numColumnas = parseInt($('#columnas').val());
+    function desMen() {
+        var canFila = parseInt($('#filas').val());
+        var canColumna = parseInt($('#columnas').val());
         var input = $('#input').val();
         var output = '';
 
-        if (input.length !== numFilas * numColumnas) {
+        if (input.length !== canFila * canColumna) {
             alert('La longitud del texto cifrado no coincide con el tamaño de la tabla.');
             return;
         }
 
         // Leer la tabla y decifrar el texto
-        for (var i = 0; i < numFilas; i++) {
-            for (var j = 0; j < numColumnas; j++) {
-                var index = j * numFilas + i;
+        for (var i = 0; i < canFila; i++) {
+            for (var j = 0; j < canColumna; j++) {
+                var index = j * canFila + i;
                 output += $('#tablaDinamica tr:eq(' + i + ') td:eq(' + j + ')').text();
             }
         }
@@ -75,4 +75,4 @@ var numFilas = 0
         $('#output').val(output);
     }
 
-    $('#filas, #columnas').on('input', generarTabla);
+    $('#filas, #columnas').on('input', tablaNueva);
